cf350b95ce787863f70224c53255a033929a94fc
egor358 <randelman32@gmail.com>
2025-12-26 14:34:13 +0200

    footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,146 @@
+import { Box, Typography } from "@mui/material";
+
+export const Footer = () => {
+  return (
+    <Box
+      sx={{
+        backgroundColor: "#000000",
+        width: "100%",
+        height: "150px",
+        marginTop: "auto",
+        display: "flex",
+      }}
+    >
+      <Box
+        sx={{
+          marginLeft: "40px",
+          width: "60%",
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Typography
+          sx={{
+            marginTop: "30px",
+            color: "white",
+            fontSize: "10px",
+            display: "flex",
+            gap: "10px",
+          }}
+        >
+          <span>Terms Of Use</span> <span>Privacy-Polici</span> <span>FAQ</span>{" "}
+          <span>Watch List</span>
+        </Typography>
+        <Typography
+          sx={{
+            color: "white",
+            fontSize: "10px",
+            marginTop: "20px",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <span>
+            2023 WATCHIT.All Rights Reserved.All videos and shows on this
+            platform are trademarks of,and all raletad images and content are
+            the property of,
+          </span>
+          <span>
+            Streamit inc.Duplication and copy of this is strycly prohibited.All
+            rights reserved
+          </span>
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          marginTop: "20px",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Typography
+          sx={{
+            fontSize: "10px",
+            color: "white",
+          }}
+        >
+          <span>Follow us:</span>
+        </Typography>
+        <Box
+          sx={{
+            marginTop: "5px",
+            display: "flex",
+            flexDirection: "row",
+            gap: "10px",
+          }}
+        >
+          <img
+            src="/img/facebook.jpg"
+            style={{
+              width: "25px",
+              height: "25px",
+            }}
+          />
+          <img
+            src="/img/twitter.png"
+            style={{
+              width: "25px",
+              height: "25px",
+            }}
+          />
+          <img
+            src="/img/googleplus.png"
+            style={{
+              width: "25px",
+              height: "25px",
+            }}
+          />
+          <img
+            src="/img/gadget.jpg"
+            style={{
+              width: "25px",
+              height: "25px",
+            }}
+          />
+        </Box>
+      </Box>
+
+      <Box
+        sx={{
+          gap:"10px",
+          marginTop: "20px",
+          marginLeft: "100px",
+        }}
+      >
+        <Typography
+          sx={{
+            color: "white",
+            fontSize: "10px",
+          }}
+        >
+          Watchhit App
+        </Typography>
+        <Box sx={{
+          display:"flex",
+          gap:"10px"
+        }}>
+        <img
+          src="/img/App-store.png"
+          style={{
+            height: "30px",
+            width: "80px",
+          }}
+        />
+        <img
+          src="/img/google-play.png"
+          style={{
+            height: "30px",
+            width: "80px",
+          }}
+        />
+        </Box>
+      </Box>
+    </Box>
+  );
+};
--- src/components/singleCard/SinglCard.jsx
+++ src/components/singleCard/SinglCard.jsx
@@ -8,11 +8,11 @@ import { Box } from "@mui/material";
 
 function SinglCard({ id, name, time, image, handleCardId }) {
   return (
-
     <Card sx={{ position: "relative", width: "395px", height: "329px" }}>
       <CardMedia
         sx={{
           position: "absolute",
+          objectFit: "contain",
         }}
         component="img"
         height="100%"
@@ -22,37 +22,35 @@ function SinglCard({ id, name, time, image, handleCardId }) {
       <Box
         sx={{
           position: "absolute",
+          objectPosition: "center",
           background:
             "linear-gradient(90deg, rgba(0, 0, 0, 0.80) 0%, rgba(20, 20, 20, 0.40) 50%, rgba(83, 100, 141, 0.00) 100%)",
-            top: 0,
+          top: 0,
           left: 0,
           width: "100%",
           height: "100%",
         }}
       />
       <CardContent
-      sx={{
-        flexDirection:'column',
-        color: "#fff",
-        position:'absolute',
-        top:'40%'
-      }}
+        sx={{
+          flexDirection: "column",
+          color: "#fff",
+          position: "absolute",
+          top: "40%",
+        }}
       >
-        <Typography 
-        gutterBottom sx={{  fontSize: 14 }}>
+        <Typography gutterBottom sx={{ fontSize: 14 }}>
           {name}
-          
         </Typography>
-        
-        <Typography 
-        gutterBottom sx={{  fontSize: 14 }}>
+
+        <Typography gutterBottom sx={{ fontSize: 14 }}>
           {time}
         </Typography>
         <Button
-          onClick={()=>handleCardId(id)}
+          onClick={() => handleCardId(id)}
           size="small"
           sx={{
-            textTransform:'none',
+            textTransform: "none",
             border: "1px solid #E50914",
             background: "#E50914",
             width: "100px",
@@ -64,7 +62,6 @@ function SinglCard({ id, name, time, image, handleCardId }) {
           Learn More
         </Button>
       </CardContent>
-      
     </Card>
   );
 }
